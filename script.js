faed569ee222204d27b6d8686e816bfcb1b97e50
Amartya Aishwarya <amartya970914@gmail.com>
2020-06-18 22:52:42 +0530

    adding todo item to incompleteTask array

--- script.js
+++ script.js
@@ -8,14 +8,18 @@ var notificationDiv = document.getElementById('notification');
 var todoItem ;
 
 
-// function to create new todo item
+// function to create new todo div item
 function createTodoItem(task){
+    var idDiv = task.toLowerCase();
+    idDiv = idDiv.replace(/\s/g, "");
+    // console.log(idDiv);
     if(totalTasks === 0)
     {
         todoContainer.classList.remove('empty-div');
         todoContainer.innerText = "";   
     }
     var newDiv = document.createElement('div');
+    // newDiv.id = idDiv;
     newDiv.classList.add('todo-item-cont', 'm-tb', 'br-5px');   // add multiple classes
     var div1 = document.createElement('div');
     var checkBox = document.createElement('input');
@@ -27,14 +31,17 @@ function createTodoItem(task){
     div1.appendChild(checkBox);
     div1.appendChild(label)
     var div2 = document.createElement('div');
-    div2.innerHTML = '<i class="fa fa-trash-o" aria-hidden="true"></i>';
+    div2.innerHTML = `<i class="fa fa-trash-o" id="${idDiv}" aria-hidden="true"></i>`;
     div2.style.paddingRight = "1.5%";
     newDiv.appendChild(div1);
     newDiv.appendChild(div2);
+    incompleteTasks.push(idDiv);
     return newDiv; 
 
 }
 
+
+//function to show notification of action done
 function showNotification(type, message){
    if(type === 'error')
    {
@@ -54,6 +61,8 @@ function showNotification(type, message){
    }, 2500);
 }
 
+
+//function creating new todo
 function createNewTodo(enteredTask) {
     if(event.key === 'Enter') {
         if(enteredTask.value === "")
@@ -62,14 +71,17 @@ function createNewTodo(enteredTask) {
             return;
         }
         todoItem = createTodoItem(enteredTask.value); 
+        setTotalTaskVal();
         console.log(todoItem);
         todoContainer.appendChild(todoItem);
         showNotification('success', 'Todo added successfully')
     }
+    
 }
 
 
 function setTotalTaskVal() {
+    totalTasks = completedTasks.length + incompleteTasks.length;
     totalTaskVal.innerHTML = totalTasks;
     if(totalTasks === 0)
     {
